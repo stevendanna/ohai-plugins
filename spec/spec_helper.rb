d9238ae3d2ce2a72951336755439199ea84dbc7c
Steven Danna <steve@opscode.com>
2013-04-19 10:29:27 -0700

    Add rspec configuration block to spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,11 @@ $:.unshift(File.join(File.dirname(__FILE__), "..", "plguins"))
 $:.unshift(File.dirname(__FILE__))
 require 'ohai'
 Ohai::Config[:plugin_path] = [File.expand_path("plugins/")]
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
